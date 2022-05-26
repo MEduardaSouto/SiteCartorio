15ecb77d2cabfb7e4e4c07b6e51831445797d63b
Eduarda Souto <eduardasouto2823@gmail.com>
2022-05-25 22:01:43 -0300

    Alterações

--- Scripts/script_inicio.js
+++ Scripts/script_inicio.js
@@ -1,14 +1,24 @@
 const login = document.getElementById("login");
 
-function redirecionar () {
+function redirecionar() {
     window.location.assign()
 }
 
 document.querySelectorAll('[palmeiras]').forEach(folder => {
-folder.onclick = function (e) {
-const ul = folder.nextElementSibling
-const d = ul.style.display
-ul.style.display = d === 'none' ? 'block' : 'none'
-}
+    folder.onclick = function (e) {
+        const ul = folder.nextElementSibling
+        const d = ul.style.display
+        ul.style.display = d === 'none' ? 'block' : 'none'
+    }
 })
-            
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+
+closeForm() 
